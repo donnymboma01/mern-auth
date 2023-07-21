12a3170cd18865cf11f4203d9f652acda1c954c5
donnymboma01 <donnymboma01@gmail.com>
2023-07-21 21:55:27 +0200

    feat: user model added

---
+++ src/models/userModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", UserSchema);
